f48884707f108925ec676404bfd30dd8016bb48f
MrLiem <liem12t@gmail.com>
2020-08-04 13:54:04 +0700

    xong 2 api get, post

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express= require('express')
+const morgan=require('morgan')
+const helmet=require('helmet')
+const cors= require('cors')
+const middlewares= require('./middlewares')
+const mongoose = require('mongoose')
+const logs=require('./api/logs')
+
+
+const app=express();
+require('dotenv').config()
+
+
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+app.use(morgan('common'))
+app.use(helmet())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.json({
+        message: "ok"
+    })
+})
+
+app.use('/api/logs',logs)
+
+app.use(middlewares.notFound)
+
+
+app.use(middlewares.errorHandler)
+
+const PORT= process.env.PORT || 1377 ;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server listen at http://localhost: ${PORT}`)
+})
